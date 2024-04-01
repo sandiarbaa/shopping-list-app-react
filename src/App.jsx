2006995eccd164dc11186969e4958d3144ad42e2
sandiarbaa <sandiarbaa@gmail.com>
2024-04-01 11:44:00 +0700

    feat(app): complete ui for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IoMdCloseCircle } from "react-icons/io";
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "kopi",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 2,
+    name: "gula",
+    quantity: 3,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "sabun",
+    quantity: 3,
+    checked: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full min-h-screen flex justify-center items-center bg-slate-200">
+      <div className="w-[350px] h-[480px] bg-white border shadow rounded-lg flex flex-col justify-between">
+        <div>
+          <h1 className="text-center mt-5 text-2xl font-bold text-orange-400">
+            Shopping List App
+          </h1>
+
+          {/* input section */}
+          <div className="w-full flex justify-center items-center my-5">
+            <form className="flex gap-x-2">
+              <select
+                name="quantity"
+                id="quantity"
+                className="px-2 py-1 bg-orange-400 rounded-md text-white hover:bg-orange-500"
+              >
+                <option value="" className="text-xs px-5">
+                  1
+                </option>
+                <option value="" className="text-xs px-5">
+                  2
+                </option>
+                <option value="" className="text-xs px-5">
+                  3
+                </option>
+              </select>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="rounded-md border border-orange-500 px-1.5 text-black outline-orange-500"
+                placeholder="Add Items.."
+              />
+              <button
+                type="submit"
+                className="px-2 py-1 bg-orange-400 rounded-md text-white hover:bg-orange-500"
+              >
+                Add
+              </button>
+            </form>
+          </div>
+
+          {/* list section */}
+          <div className="w-full mx-auto">
+            <ol className="px-5">
+              {groceryItems.map((item) => (
+                <li
+                  key={item.id}
+                  className=" text-xl text-center flex items-center gap-x-2 py-3 border-y-[1.5px]"
+                >
+                  <input
+                    type="checkbox"
+                    name="checkItem"
+                    id="checkItem"
+                    // className="text-2xl"
+                    style={{ width: "20px", height: "30px" }}
+                  />
+                  {item.name}
+                  <IoMdCloseCircle />
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+
+        {/* Sort */}
+        <div className="my-5 w-full flex justify-center items-center gap-x-3">
+          <span className="text-lg">Sort by : </span>
+          <select
+            name="quantity"
+            id="quantity"
+            className="px-2 py-1 w-40 bg-orange-400 rounded-md text-white hover:bg-orange-500"
+          >
+            <option value="" className="text-xs px-5">
+              input order
+            </option>
+            <option value="" className="text-xs px-5">
+              item name
+            </option>
+            <option value="" className="text-xs px-5">
+              checklist
+            </option>
+          </select>
+          <span className="text-lg bg-orange-400 px-2 py-1 rounded-md text-white hover:bg-orange-500">
+            Clear list
+          </span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
